e3ceb7304f96c95cc98233d601712aa0f39d8dc5
daniloxxv <danilosoares@gmail.com>
2020-07-18 13:32:40 +0100

    Testing insertion sort

--- __tests__/sorting.test.js
+++ __tests__/sorting.test.js
@@ -1,33 +1,35 @@
-const expect = require('expect')
-const bubbleSort = require('../sorting-algorithms/bubble_sort')
-const mergeSort = require('../sorting-algorithms/merge_sort')
-const selectionSort = require('../sorting-algorithms/selection_sort')
-const quickSort = require('../sorting-algorithms/quick_sort')
-const radixSort = require('../sorting-algorithms/radix_sort')
+const expect = require('expect');
+const bubbleSort = require('../sorting-algorithms/bubble_sort');
+const mergeSort = require('../sorting-algorithms/merge_sort');
+const selectionSort = require('../sorting-algorithms/selection_sort');
+const insertionSort = require('../sorting-algorithms/insertion-sort');
+const quickSort = require('../sorting-algorithms/quick_sort');
+const radixSort = require('../sorting-algorithms/radix_sort');
 
-describe('Sorting algorithms', ()=>{
-    const algorithms = {
-        bubbleSort,
-        mergeSort,
-        selectionSort, 
-        quickSort,
-        radixSort
-    }
+describe('Sorting algorithms', () => {
+  const algorithms = {
+    bubbleSort,
+    mergeSort,
+    selectionSort,
+    insertionSort,
+    quickSort,
+    radixSort,
+  };
 
-    for (const key in algorithms){
-        it(`Sorts a simple array with ${key}`, ()=>{
-            expect(algorithms[key]([3,2,1])).toEqual([1,2,3])
-        })
-        it(`Handles an empty array with ${key}`, ()=>{
-            expect(algorithms[key]([])).toEqual([])
-        })
-        it(`Handles a previously sorted array with ${key}`, ()=>{
-            const sortedArray = Array.from({length: 5000}, (_,i) => i)
-            expect(algorithms[key](sortedArray)).toEqual([...sortedArray].sort((a,b)=>a-b))
-        })
-        it(`Sorts a random array with ${key}`,()=>{
-            const randomArray = Array.from({length: 5000}, () => Math.floor(Math.random()*5000))
-            expect(algorithms[key]([...randomArray])).toEqual([...randomArray].sort((a,b)=>a-b))
-        })
-    }
-})
+  Object.keys(algorithms).forEach((key) => {
+    it(`Sorts a simple array with ${key}`, () => {
+      expect(algorithms[key]([3, 2, 1])).toEqual([1, 2, 3]);
+    });
+    it(`Handles an empty array with ${key}`, () => {
+      expect(algorithms[key]([])).toEqual([]);
+    });
+    it(`Handles a previously sorted array with ${key}`, () => {
+      const sortedArray = Array.from({ length: 5000 }, (_, i) => i);
+      expect(algorithms[key](sortedArray)).toEqual([...sortedArray].sort((a, b) => a - b));
+    });
+    it(`Sorts a random array with ${key}`, () => {
+      const randomArray = Array.from({ length: 5000 }, () => Math.floor(Math.random() * 5000));
+      expect(algorithms[key]([...randomArray])).toEqual([...randomArray].sort((a, b) => a - b));
+    });
+  });
+});
--- sorting-algorithms/insertion-sort.js
+++ sorting-algorithms/insertion-sort.js
@@ -1,10 +1,13 @@
-function insertionSort(arr){
-  for (let i = 0, j = 1; j < arr.length; j++, i = j - 1){
+function insertionSort(arr) {
+  for (let i = 0, j = 1; j < arr.length; j += 1, i = j - 1) {
     const key = arr[j];
-    while (i >= 0 && arr[i] > key){
-      arr[i+1] = arr[i];
-      i--;
+    while (i >= 0 && arr[i] > key) {
+      arr[i + 1] = arr[i];
+      i -= 1;
     }
-    arr[i+1] = key;
+    arr[i + 1] = key;
   }
+  return arr;
 }
+
+module.exports = insertionSort;
