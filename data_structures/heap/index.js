7dc307e61c12e438b3b157d29a7b1a4590f26671
daniloxxv <danilosoares@gmail.com>
2020-07-20 20:59:25 +0100

    Refactored for readability

--- data_structures/heap/index.js
+++ data_structures/heap/index.js
@@ -11,6 +11,33 @@ class Heap {
   }
 
   /**
+   * Gets the index of the parent node of an element in a given index
+   * @param {Number} index The index
+   * @returns {Number} The parent node index
+   */
+  static getParent(index) {
+    return Math.floor((index - 1) / 2);
+  }
+
+  /**
+   * Gets the index of the left child of an element in a given index
+   * @param {Number} index The index
+   * @returns {Number} The left child index
+   */
+  static getLeftChild(index) {
+    return 2 * index + 1;
+  }
+
+  /**
+   * Gets the index of the right child of an element in a given index
+   * @param {Number} index The index
+   * @returns {Number} The right child index
+   */
+  static getRightChild(index) {
+    return 2 * index + 2;
+  }
+
+  /**
    * Inserts a value into the binary heap
    * @param {Number} value The value to insert
    */
@@ -18,12 +45,12 @@ class Heap {
     // Adds an element to the end of the heap
     this.values.push(value);
     let index = this.values.length - 1;
-    let parentIndex = Math.floor((index - 1) / 2);
     // Compares the element to its parent and swaps until it's in the right place
-    while (value > this.values[parentIndex] && index > 0) {
+    while (index > 0) {
+      const parentIndex = Heap.getParent(index);
+      if (this.values[index] < this.values[parentIndex]) break;
       swap(this.values, index, parentIndex);
       index = parentIndex;
-      parentIndex = Math.floor((index - 1) / 2);
     }
   }
 
@@ -35,18 +62,19 @@ class Heap {
     // Swaps the first and last elements
     swap(this.values, 0, this.values.length - 1);
 
-    // Bubbles the new root down the tree until it finds its correct spot
     let index = 0;
     let leftChildIndex = 1;
     let rightChildIndex = 2;
+
+    // Swaps the new root with its greatest child until it finds its correct spot
     while (this.values[index] < this.values[leftChildIndex]
         || this.values[index] < this.values[rightChildIndex]) {
       const greatestChildIndex = (this.values[leftChildIndex] > this.values[rightChildIndex])
         ? leftChildIndex : rightChildIndex;
       swap(this.values, index, greatestChildIndex);
       index = greatestChildIndex;
-      leftChildIndex = 2 * index + 1;
-      rightChildIndex = 2 * index + 2;
+      leftChildIndex = Heap.getLeftChild(index);
+      rightChildIndex = Heap.getRightChild(index);
     }
     return this.values.pop();
   }
